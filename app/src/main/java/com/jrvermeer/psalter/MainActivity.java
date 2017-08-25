ed3487896294478582c351ea84f5931859b7a5f1
jrvermee <vermeer.jonathan@gmail.com>
2017-08-24 22:42:00 -0400

    fix crash on start

--- app/src/main/java/com/jrvermeer/psalter/MainActivity.java
+++ app/src/main/java/com/jrvermeer/psalter/MainActivity.java
@@ -15,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
